74d15f32d0c0ad587f650aa46e702501a8e18862
Anggunramadhani <anggung.ramadhani@gmail.com>
2024-03-10 20:28:20 +0700

    firstcommit

---
+++ tugasobj1/src/Main.java
@@ -0,0 +1,18 @@
+public class Main{
+    public static void main(String[] args) {
+        kucing cat =new kucing();
+        kucing.Nama();
+        kucing.Makananfavorit();
+        kucing.warna();
+        kucing.umur();
+        kucing.berattubuh();
+    }
+
+
+
+
+
+
+
+
+}
